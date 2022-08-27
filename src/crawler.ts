c172e8f388801da327399b7d2bedf58e0aa42bf2
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-08-27 11:47:52 +0900

    fix: text

--- src/crawler.ts
+++ src/crawler.ts
@@ -3,7 +3,7 @@ import {
   ButtonBuilder,
   ButtonStyle,
   Client,
-  TextChannel,
+  TextChannel
 } from 'discord.js'
 import { TwitterApi } from 'twitter-api-v2'
 import { Config, Notified, Target } from './utlis'
@@ -16,7 +16,7 @@ export default class Crawler {
   constructor(config: Config, client: Client, target: Target) {
     this.client = new TwitterApi({
       appKey: config.twitter.consumerKey,
-      appSecret: config.twitter.consumerSecret,
+      appSecret: config.twitter.consumerSecret
     })
     this.target = target
 
@@ -34,7 +34,7 @@ export default class Crawler {
     const favorites = await this.client.v1.favoriteTimeline(
       this.target.twitterId,
       {
-        count: 200,
+        count: 200
       }
     )
     const tweets = favorites.tweets
@@ -77,9 +77,9 @@ export default class Crawler {
           embeds: [
             {
               author: {
-                name: tweet.user.name + ' (@' + tweet.user.screen_name + ')',
-                url: 'https://twitter.com/' + tweet.user.screen_name,
-                icon_url: tweet.user.profile_image_url_https,
+                name: `${tweet.user.name} (@${tweet.user.screen_name})`,
+                url: `https://twitter.com/${tweet.user.screen_name}`,
+                icon_url: tweet.user.profile_image_url_https
               },
               description: tweet.text,
               url: tweetUrl,
@@ -88,24 +88,24 @@ export default class Crawler {
                 {
                   name: 'Retweet',
                   value: tweet.retweet_count.toString(),
-                  inline: true,
+                  inline: true
                 },
                 {
                   name: 'Likes',
                   value: tweet.favorite_count.toString(),
-                  inline: true,
-                },
+                  inline: true
+                }
               ],
               image: {
-                url: tweet.entities.media[0].media_url_https,
+                url: tweet.entities.media[0].media_url_https
               },
               footer: {
-                text: 'Twitter by ' + this.target.name + ' likes',
+                text: `Twitter by ${this.target.name} likes`
               },
-              timestamp: new Date(tweet.created_at).toISOString(),
-            },
+              timestamp: new Date(tweet.created_at).toISOString()
+            }
           ],
-          components: [row],
+          components: [row]
         })
       }
       Notified.addNotified(this.target.twitterId, tweet.id_str)
