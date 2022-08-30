2c9aae6331800f05d9df4a4f1f00c455f82fd483
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-08-30 14:11:37 +0900

    chore: debugging

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,7 @@ client.on('ready', async () => {
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isButton()) return
+  console.log("interactionCreate@Button: " + interaction.id)
   if (interaction.customId.startsWith('favorite-')) {
     if (interaction.user.id !== config.discord.ownerId) {
       await interaction.reply({
